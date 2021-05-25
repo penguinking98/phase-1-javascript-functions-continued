b95addb7ded251fb86e374cd7a0eccf7f1a83c6e
Ben Kramer <benkramer614@gmail.com>
2021-05-25 10:43:20 -0400

    commit tree a452343f6c160861f887e1514fb949f729146553

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function saturdayFun(activity){
+        return 'This Saturday, I want to roller-skate';
+}
+
+function mondayWork(){
+    return 'This Monday, I will go to the office.';
+}
+
+function wrapAdjective() {
+    const part1 = 'blank'
+    return function(){
+        const part2 = 'blanc'
+    }
+    return 'You are %a dedicated programmer%!';
+
+
+
+}
